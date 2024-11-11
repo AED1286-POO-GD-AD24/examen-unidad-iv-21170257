c91e667ec9d6472d0ceb0c0d2a254418a3d6fe98
bryant jared bejarano lizarraga <L21170257@culiacan.tecnm.mx>
2024-11-11 16:47:17 +0000

    terminado

--- clases/ProductoElectronico.java
+++ clases/ProductoElectronico.java
@@ -1,27 +1,9 @@
 package clases;
 
 
-public class ProductoElectronico extends Producto 
-{
+public class ProductoElectronico extends Producto{
 
-    
-    public ProductoElectronico(String nombre, double precio) 
-    {
+    public ProductoElectronico(String nombre, double precio){
         super(nombre, precio);
     }
-
-    
-    @Override
-    public String getNombre() 
-    {
-        return super.nombre;  
-    }
-
-    @Override
-    public double getPrecio() 
-    {
-        return super.precio;  
-    }
 }
-
-
