c6536bde24988ec43d6941cb1142627c47015edc
bryant jared bejarano lizarraga <L21170257@culiacan.tecnm.mx>
2024-11-11 16:52:35 +0000

    final

--- clases/ProductoAlimenticio.java
+++ clases/ProductoAlimenticio.java
@@ -1,22 +1,11 @@
 package clases;
 
-public class ProductoAlimenticio extends Producto 
+
+public class ProductoAlimenticio extends Producto
 {
 
-    public ProductoAlimenticio(String nombre, double precio) 
+    public ProductoAlimenticio(String nombre, double precio)
     {
         super(nombre, precio);
-    }
-
-    @Override
-    public String getNombre() 
-    {
-        return super.nombre;  
-    }
-
-    @Override
-    public double getPrecio() 
-    {
-        return super.precio;  
     }
 }
